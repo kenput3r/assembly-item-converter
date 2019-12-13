580315774b133b3d9c1568286fe8aac44307bd7a
Kenneth Lyerly <kenput3r@gmail.com>
2019-12-12 18:01:04 -0800

    rewrite to handle non bom files

--- app.js
+++ app.js
@@ -1,41 +1,44 @@
-const products_file = process.argv[2];
-const bom_file = process.argv[3];
 const csv = require('csvtojson');
 const utils = require('./utils');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 const csvWriter = createCsvWriter({
   path: './files/output.csv',
-  header: utils.ObjectHeader
+  header: utils.Headers
 });
+
 const records = {};
 
 (async () => {
-  const products = await csv().fromFile(`./files/${products_file}`);
-  const bom = await csv().fromFile(`./files/${bom_file}`);
-  //loop over products file
+  const products = await csv().fromFile(`./files/${process.argv[2]}`);
+  const bom = await csv().fromFile(`./files/${process.argv[3]}`);
+  const shopify_wholesale_file = await csv().fromFile(`./files/${process.argv[4]}`);
+  const shopify_retail_file = await csv().fromFile(`./files/${process.argv[5]}`);
+  const shopify_retail_items = utils.FormatShopifyObject(shopify_retail_file);
+  const shopify_wholesale_items = utils.FormatShopifyObject(shopify_wholesale_file);
+
   for(let i = 0; i < products.length; i++) {
     const product = products[i];
-    //loop over bill of materials file
-    BOMitemSearch:
-    for(let x = 0; x < bom.length; x++) {
-      const bom_item = bom[x];
-      if(product.ProductCode === bom_item.ProductSKU) {
-        //record already exists
-        if(records[product.ProductCode]) {
-          //find and fill first blank component item & qty column
-          ComponentColumnSearch:
-          for(let y = 1; y <= 30; y++) {
-            if(!records[product.ProductCode][`Component ${y} - Item`]) {
-              records[product.ProductCode][`Component ${y} - Item`] = bom_item.ComponentSKU;
-              records[product.ProductCode][`Component ${y} - Quantity`] = bom_item.Quantity;
-              break ComponentColumnSearch;
+    if(product[`BillOfMaterial`] === 'Yes') {
+      for(let x = 0; x < bom.length; x++) {
+        const bom_item = bom[x];
+        if(product[`ProductCode`] === bom_item[`ProductSKU`]) {
+          if(records[product[`ProductCode`]]) {
+            for(let y = 1; y <= 30; y++) {
+              if(!records[product[`ProductCode`]][`Component ${y} - Item`]) {
+                records[product[`ProductCode`]][`Component ${y} - Item`] = bom_item.ComponentSKU;
+                records[product[`ProductCode`]][`Component ${y} - Quantity`] = bom_item.Quantity;
+                break;
+              }
             }
+          }else{
+            records[product.ProductCode] = utils.ReturnObject(product, shopify_retail_items, shopify_wholesale_items);
+            records[product.ProductCode][`Component 1 - Item`] = bom_item.ComponentSKU;
+            records[product.ProductCode][`Component 1 - Quantity`] = bom_item.Quantity;
           }
-        //record doesn't exist yet
-        }else{
-          records[product.ProductCode] = utils.CreateObject(product, bom_item);
         }
       }
+    }else{
+      records[product.ProductCode] = utils.ReturnObject(product, shopify_retail_items, shopify_wholesale_items);
     }
   }
   //convert records object to array
